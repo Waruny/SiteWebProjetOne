5dfa2e79d11825592e11078031fdefd06b6f5f44
Waruny Rajendran <w.rajendran@ecole-ipssi.net>
2019-10-04 10:52:48 +0200

    update page inscription

--- php/inscription.php
+++ php/inscription.php
@@ -104,7 +104,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <li><a href="../html/services.html">Nos produits</a></li>
                 <li><a href="../html/franchise.html">Franchisés</a></li>
                 <li><a href="../php/inscription.php">Nous rejoindre</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="../html/contact.html">Contact</a></li>
             </ul>
         </nav>
     </div>
@@ -114,22 +114,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Identifiant</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <input type="text" name="username" class="form-control" placeholder="Identifiant" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Mot de passe</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <input type="password" name="password" class="form-control" placeholder="Mot de passe" value="<?php echo $password; ?>">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Confirmation mot de passe</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <input type="password" name="confirm_password" class="form-control" placeholder="Mot de passe" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Adresse mail</label>
-                <input type="mail" name="mail" class="form-control" value="<?php echo $mail; ?>">
+                <input type="mail" name="mail" class="form-control" placeholder="Mail" value="<?php echo $mail; ?>">
                 <span class="help-block"><?php echo $mail_err; ?></span>
             </div>
             <div class="form-group">
